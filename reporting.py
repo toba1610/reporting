3bb1df876feb1fac7d77f12b4f2cb34df4c61d2a
toba1610 <toba1610@googlemail.com>
2024-05-11 14:21:33 +0200

    changed to headless mode and moved to function

--- reporting.py
+++ reporting.py
@@ -1,12 +1,14 @@
 import subprocess
 import psutil
 import time
+import json_data_process as jdp
 
 
 def open_libreoffice():
-    libreoffice_path = "D:\LibreOffice\program\swriter.exe"
+    libreoffice_path = jdp.read_JSON_Data("config.json", "Config_Program", "Open_Office_Path")
 
-    subprocess.Popen([libreoffice_path])
+    # subprocess.Popen([libreoffice_path])
+    subprocess.Popen([libreoffice_path, "--headless", "--accept=\"socket,port=2002;urp;\""])
 
 
 def close_libreoffice():
@@ -16,6 +18,7 @@ def close_libreoffice():
             process.terminate()  # Terminate the process
             break
 
+open_libreoffice()
 
 time.sleep(10)
 
